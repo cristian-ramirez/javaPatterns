93981d3a5ec124d2746846ac76521e3265a4a213
Cristian Ramirez <ramireze4@gmail.com>
2016-05-18 22:19:04 -0600

    Fancy print

--- decorador/Main.java
+++ decorador/Main.java
@@ -7,27 +7,31 @@ package decorador;
 
 public class Main {
      public static void main(String[] args) {
-
+        System.out.println("========== Alienware ==========");
         Component component1 = new Alienware();
         component1 = new BlueRay(component1);
         component1 = new SSD(component1);
         
         System.out.println(component1.getDescription());
         System.out.println("Price: " + component1.price());
-
+        System.out.println("================================");
+        
+        System.out.println("============= Asus =============");
         Component component2 = new Asus();
         component2 = new BlueRay(component2);
         component2 = new SSD(component2);
         
         System.out.println(component2.getDescription());
         System.out.println("Price: " + component2.price());
+        System.out.println("================================");
         
+        System.out.println("============== HP ==============");
         Component component3 = new HP();
         component3 = new BlueRay(component3);
         component3 = new SSD(component3);
         
         System.out.println(component3.getDescription());
         System.out.println("Price: " + component3.price());
-        
+        System.out.println("================================");
     }
 }
